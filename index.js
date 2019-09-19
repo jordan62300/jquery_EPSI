90fd0a1df82de6fcb7d13ad62380393bb920617e
jordan62300 <jordanfievetpro@outlook.fr>
2019-09-19 19:44:17 +0200

    ajout des testes de mdp

--- index.js
+++ index.js
@@ -1,14 +1,45 @@
+    var passwordInput;
+    var confirmedPasswordInput;
 
    $("#age").keyup(function() {
        var majorite = $("#age").val();
        console.log(majorite)
        if(majorite < 18) {
-        $(".majorite").text("vous etes mineur")
-        $(".majorite").fadeIn("slow")
-        $(".majorite").removeClass("d-none")
+            $(".majorite").text("vous etes mineur")
+            $(".majorite").fadeIn("slow")
+            $(".majorite").removeClass("d-none")
+            $(".majorite").addClass("text-warning")
         
-       } else {
-           $(".majorite").text("Vous etes majeur")
+       } else if(majorite >= 18) {
+            $(".majorite").removeClass("text-warning")
+            $(".majorite").text("Vous etes majeur")
+            $(".majorite").fadeIn("slow")
+            $(".majorite").removeClass("d-none")
+            $(".majorite").addClass("text-success")
        }
-   }
-   )
+   })
+
+   $("#password").change(function() {
+       passwordInput = $("#password").val();
+       console.log(passwordInput);
+       if(passwordInput != confirmedPasswordInput && passwordInput != "" && confirmedPasswordInput != "" ) {
+            $(".checkpassword").text(" Les 2 mots de passes ne correspondent pas")
+            console.log("pas hello")
+    } else {
+            console.log("hello")
+            $(".checkpassword").text(" ")
+    }
+ })
+
+   $("#confirmpassword").change(function() {
+        confirmedPasswordInput = $("#confirmpassword").val();
+        console.log(confirmedPasswordInput);
+    if(passwordInput != confirmedPasswordInput && passwordInput != "" && confirmedPasswordInput != "") {
+        $(".checkpassword").text(" Les 2 mots de passes ne correspondent pas")
+        console.log("pas hello")
+    } else {
+        console.log("hello")
+        $(".checkpassword").text(" ")
+    }
+})
+
