7dad9392151bb1c13690b0ccccc1fc94c4c75bdc
jordan62300 <jordanfievetpro@outlook.fr>
2019-09-19 22:57:38 +0200

    formulaire et annimation complet

--- index.js
+++ index.js
@@ -8,25 +8,27 @@
         createProfil()
     })
 
-function isMajor() {
+function isMajor() {           // Affiche si la personne est majeur ou mineur
    $("#age").keyup(function() {
         age = $("#age").val();
-       if(age == 0 && age == "") {
-            $(".majorite").hide()
+       if(age == 0 ) {
+            $(".majorite").text(" ")
+            $(".majorite").animate({fontSize: '1em'}, "fast");
        } else if(age >= 18 ) {
             $(".majorite").removeClass("text-warning")
             $(".majorite").text("Vous etes majeur")
-            $(".majorite").fadeIn("slow").removeClass('d-none');
+            $(".majorite").animate({fontSize: '1em'}, "slow");
             $(".majorite").addClass("text-success")
+
        } else if(age < 18) {
-            $(".majorite").fadeIn('slow').removeClass('d-none');
+            $(".majorite").animate({fontSize: '3em'}, "slow");
             $(".majorite").text("vous etes mineur")
             $(".majorite").addClass("text-warning")
        }
    })
 }
 
-function verificationPassword() {
+function verificationPassword() {       // verifie si le mot de passe saisie et égal au mot de passe de confirmation
     $("#password").change(function() {
         if($("#password").val() != $("#confirmpassword").val() && $("#password").val() != "" && $("#confirmpassword").val() != "" ) {
                 $(".checkpassword").text(" Les 2 mots de passes ne correspondent pas")
@@ -43,7 +45,7 @@ function verificationPassword() {
     })
 }
 
-function createProfil() {
+function createProfil() {           // Permet de creer un profil en appuyant sur le button envoyer et affiche les informations en dessous du formulaire 
     $(".send").click(function() {
         prenom = $("#prenom").val();
         nom = $("#nom").val();
